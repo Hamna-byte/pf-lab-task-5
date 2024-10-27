e946dc10386bfc5727a5bce8c49670105463ac22
Hamna-byte <yousafsaadi564@gmail.com>
2024-10-27 21:34:27 +0000

    Add project files.

---
+++ pf lab task 5/pf lab task 5.cpp
@@ -0,0 +1,14 @@
+// pf lab task 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+   for( int i=10; i>=1; i--)
+   {
+	   cout << i << " ";
+   }
+   return 0;
+}
+
